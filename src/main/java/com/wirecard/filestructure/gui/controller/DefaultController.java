f7fb669337ff3b91a4df19c8b1100128e93a363a
wulantika.nuraeni <wulantika.nuraeni@wirecard.com>
2020-02-07 16:16:59 +0700

    Add default controller

--- src/main/java/com/wirecard/filestructure/gui/controller/DefaultController.java
+++ src/main/java/com/wirecard/filestructure/gui/controller/DefaultController.java
@@ -1,4 +1,17 @@
 package com.wirecard.filestructure.gui.controller;
 
-public class DefaultController {
+import java.awt.*;
+
+public class DefaultController extends AbstractController {
+
+    public static final String ELEMENT_TEXT_PROPERTY = "Text";
+    public static final String ELEMENT_FONT_PROPERTY = "Font";
+
+    public void changeElementText(String newText){
+        setModelProperty(ELEMENT_TEXT_PROPERTY, newText);
+    }
+
+    public void changeElementFont(Font newFont){
+        setModelProperty(ELEMENT_FONT_PROPERTY, newFont);
+    }
 }
